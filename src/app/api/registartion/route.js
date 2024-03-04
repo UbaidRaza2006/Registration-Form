05c27b43acd4a9e0e3599173eedffdaf66d28950
codemutahir <mutahir.btkit@gmail.com>
2024-03-04 11:51:14 +0500

    The API endpoint http://localhost:3000/api/students?rollNo=57611 will provide data for a single student with the roll number 57611.
    Alternatively, the API endpoint http://localhost:3000/api/students will return data for all students.
    
    In Next.js, when req.query is not directly available, you can access query parameters using const query = new URL(req.url).searchParams.get("rollNo"); This method enables you to retrieve the roll number from the URL query parameters.

--- src/app/api/registartion/route.js
+++ src/app/api/registartion/route.js
@@ -1,8 +1,7 @@
-
 // import Register from "@/models/registration";
 import { data } from "autoprefixer";
 // const { v4: uuidv4 } = require('uuid');
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 import Joi from "joi";
 import { connect } from "mongoose";
@@ -11,184 +10,167 @@ import connectToDb from "../../../database";
 import Register from "../../../models/registration";
 import { NextResponse } from "next/server";
 
-
-
-
 const RegistrationSchema = Joi.object({
-    fullName: Joi.string().required(),
-    fatherName: Joi.string().required(),
-    email: Joi.string().required(),
-    course: Joi.string().required(),
-    batch: Joi.string().required(),
-    rollNo:Joi.string().required(),
-    payment:Joi.string().required(),
-    status:Joi.string().required(),
-    city: Joi.string().required(),
-    cnic: Joi.string().required(),
-    phone: Joi.string().required(),
-    dateOfBirth: Joi.string().required(),
-    gender: Joi.string().required(),
-    qualification: Joi.string().required(),
-    address: Joi.string().required(),
-    imageUrl: Joi.string().required()
-})
+  fullName: Joi.string().required(),
+  fatherName: Joi.string().required(),
+  email: Joi.string().required(),
+  course: Joi.string().required(),
+  batch: Joi.string().required(),
+  rollNo: Joi.string().required(),
+  payment: Joi.string().required(),
+  status: Joi.string().required(),
+  city: Joi.string().required(),
+  cnic: Joi.string().required(),
+  phone: Joi.string().required(),
+  dateOfBirth: Joi.string().required(),
+  gender: Joi.string().required(),
+  qualification: Joi.string().required(),
+  address: Joi.string().required(),
+  imageUrl: Joi.string().required(),
+});
 
-
-
-export const dynamic = "force-dynamic"
-
-
-
+export const dynamic = "force-dynamic";
 
 export async function POST(req) {
-    try {
-        await connectToDb();
+  try {
+    await connectToDb();
 
-        
+    const extractData = await req.json();
+    const {
+      fullName,
+      fatherName,
+      email,
+      course,
+      batch,
+      payment,
+      status,
+      city,
+      cnic,
+      phone,
+      dateOfBirth,
+      gender,
+      qualification,
+      address,
+      imageUrl,
+    } = extractData;
 
-            const extractData = await req.json();
-            const {
-                 fullName,
-                fatherName,
-                email,
-                course,
-                batch,
-                payment,
-                status,
-                city,
-                cnic,
-                phone,
-                dateOfBirth,
-                gender,
-                qualification,
-                address,
-                imageUrl}= extractData;
+    const generateRandomNumber = () =>
+      Math.floor(10000 + Math.random() * 90000);
+    const generatedRollNo = generateRandomNumber();
 
-                const generateRandomNumber = () => Math.floor(10000 + Math.random() * 90000);
-                const generatedRollNo=generateRandomNumber()
+    // const { error } = RegistrationSchema.validate({
+    //     fullName,
+    //     fatherName,
+    //     email,
+    //     rollNo:generatedRollNo.toString(),
+    //     course,
+    //     batch,
+    //     payment,
+    //     status,
+    //     city,
+    //     cnic,
+    //     phone,
+    //     dateOfBirth,
+    //     gender,
+    //     qualification,
+    //     address,
+    //     imageUrl
+    // })
 
-            // const { error } = RegistrationSchema.validate({
-            //     fullName,
-            //     fatherName,
-            //     email,
-            //     rollNo:generatedRollNo.toString(),
-            //     course,
-            //     batch,
-            //     payment,
-            //     status,
-            //     city,
-            //     cnic,
-            //     phone,
-            //     dateOfBirth,
-            //     gender,
-            //     qualification,
-            //     address,
-            //     imageUrl
-            // })
+    // if (error) {
+    //     return NextResponse.json({
+    //         success: false,
+    //         message: error.details[0].message,
+    //     })
+    // }
 
-            // if (error) {
-            //     return NextResponse.json({
-            //         success: false,
-            //         message: error.details[0].message,
-            //     })
-            // }
+    // // const sameCnic=await Register.findOne({cnic:cnic})
 
-            // // const sameCnic=await Register.findOne({cnic:cnic})
+    // // if(sameCnic){
 
-            // // if(sameCnic){
+    // //     return NextResponse.json({
+    // //         success: false,
+    // //         message: "Student from this Cnic/B-form is already registered!",
+    // //     })
 
-            // //     return NextResponse.json({
-            // //         success: false,
-            // //         message: "Student from this Cnic/B-form is already registered!",
-            // //     })
+    // // }
 
-            // // }
+    // const newlyRegisteredUser = await Register.create({
+    //     ...extractData,
+    //     rollNo: generatedRollNo.toString(),
+    //  } )
 
-            // const newlyRegisteredUser = await Register.create({
-            //     ...extractData,
-            //     rollNo: generatedRollNo.toString(),
-            //  } )
+    const { error } = RegistrationSchema.validate({
+      fullName,
+      fatherName,
+      email,
+      course,
+      batch,
+      payment,
+      status,
+      city,
+      cnic,
+      phone,
+      dateOfBirth,
+      gender,
+      qualification,
+      address,
+      imageUrl,
+      rollNo: generatedRollNo.toString(),
+    });
 
-
-            const { error } = RegistrationSchema.validate({
-                fullName,
-                fatherName,
-                email,
-                course,
-                batch,
-                payment,
-                status,
-                city,
-                cnic,
-                phone,
-                dateOfBirth,
-                gender,
-                qualification,
-                address,
-                imageUrl,
-                rollNo: generatedRollNo.toString()
-            });
-    
-            if (error) {
-                return NextResponse.json({
-                    success: false,
-                    message: error.details[0].message,
-                });
-            }
-    
-            const newlyRegisteredUser = await Register.create({
-                fullName,
-                fatherName,
-                email,
-                course,
-                batch,
-                payment,
-                status,
-                city,
-                cnic,
-                phone,
-                dateOfBirth,
-                gender,
-                qualification,
-                address,
-                imageUrl,
-                rollNo: generatedRollNo.toString(),
-            });
-
-            if(newlyRegisteredUser){
-                console.log(newlyRegisteredUser);
-                return NextResponse.json({
-                    success: true,
-                    user:newlyRegisteredUser,
-                    message: "Registered Successfully!",
-                })
-            }else{
-                return NextResponse.json({
-                    success: false,
-                    message: "Failed to register. Plz try again! ",
-                })
-            }
-
-
-
-
-        
-
-
-
-    } catch (error) {
-        // if (error.code === 11000) {
-        //     // Duplicate key error, handle it
-        //     return NextResponse.json({
-        //         success: false,
-        //         message: "A user with the same CNIC already exists.",
-        //     });
-        // }
-        console.log("Error in adding new user-->", error);
-
-        return NextResponse.json({
-            success: false,
-            message: "Something went Wrong, please Try Again later!"
-        })
+    if (error) {
+      return NextResponse.json({
+        success: false,
+        message: error.details[0].message,
+      });
     }
+
+    const newlyRegisteredUser = await Register.create({
+      fullName,
+      fatherName,
+      email,
+      course,
+      batch,
+      payment,
+      status,
+      city,
+      cnic,
+      phone,
+      dateOfBirth,
+      gender,
+      qualification,
+      address,
+      imageUrl,
+      rollNo: generatedRollNo.toString(),
+    });
+
+    if (newlyRegisteredUser) {
+      console.log(newlyRegisteredUser);
+      return NextResponse.json({
+        success: true,
+        user: newlyRegisteredUser,
+        message: "Registered Successfully!",
+      });
+    } else {
+      return NextResponse.json({
+        success: false,
+        message: "Failed to register. Plz try again! ",
+      });
+    }
+  } catch (error) {
+    // if (error.code === 11000) {
+    //     // Duplicate key error, handle it
+    //     return NextResponse.json({
+    //         success: false,
+    //         message: "A user with the same CNIC already exists.",
+    //     });
+    // }
+    console.log("Error in adding new user-->", error);
+
+    return NextResponse.json({
+      success: false,
+      message: "Something went Wrong, please Try Again later!",
+    });
+  }
 }
