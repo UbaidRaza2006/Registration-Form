ca50387db2e357c354dfa44317f8d096c6287fef
ubaidraza2006 <ubaidmemon2006@gmail.com>
2024-12-13 16:35:09 +0500

    new svg in id card and content written hardcoated

--- src/components/IdCard/index.js
+++ src/components/IdCard/index.js
@@ -78,12 +78,15 @@ function IdCard({ user, contentImage }) {
       </Button>
 
       <div style={{ position: "absolute", left: "-9999px", top: "-9999px" }}>
-        <div ref={idCardRef} className="relative pt-2 w-[350px] h-[500px]">
-          {/* First child div for the school image */}
-          <div className="mx-auto w-[180px] h-[120px] relative">
+      <div ref={idCardRef} className="relative pt-1 w-[300px] h-[450px]">
+  {/* Top Heading */}
+  <h1 className="text-center text-xs font-serif font-semibold text-[#007076] mb-1">YOUR ID CARD</h1>
+
+  {/* ID Card Section */}
+  <div className="mx-auto w-[180px] h-[120px] relative">
             <div className="absolute w-[177px] h-[116px] border border-gray-900">
               <Image
-                src="/images/Green Minimalist School ID Card.svg"
+                src="/images/Green Minimalist School ID Card (5).svg"
                 className="w-full h-full object-cover"
                 width={600}
                 height={400}
@@ -104,15 +107,9 @@ function IdCard({ user, contentImage }) {
 
               {/* User Details */}
               <div className="absolute top-0 left-0 w-[37%] h-[35%] mt-[35%] ml-[53%] overflow-hidden">
-                <p className=" id-card-text">
-                  {user.fullName}
-                </p>
-                <p className=" id-card-text">
-                  {user.course}
-                </p>
-                <p className="id-card-text">
-                  {user.batch}
-                </p>
+                <p className=" id-card-text font-poppins font-semibold">{user.fullName}</p>
+                <p className=" id-card-text font-poppins font-semibold">{user.course}</p>
+                <p className="id-card-text font-poppins font-semibold">{user.batch}</p>
               </div>
 
               {/* User Roll Number */}
@@ -133,20 +130,33 @@ function IdCard({ user, contentImage }) {
             </div>
           </div>
 
-          {/* Content Image Section */}
-          <div className="w-[95%] h-[70%] mt-[10px] mx-auto">
-            {/* Add the Content Image here */}
-            {contentImage && (
-              <Image
-                src={contentImage}
-                className="w-full h-full object-cover"
-                alt="Content Image"
-                width={600}
-                height={400}
-              />
-            )}
-          </div>
-        </div>
+  {/* Payment Information */}
+  <div className="mt-2 py-1 px-3 text-xs text-gray-600">
+    <p className="text-center text-sm font-semibold font-serif text-[#007076] mb-1">PAYMENT METHOD</p>
+
+    <p className="text-[9px] font-semibold text-[#007076] mb-1 inline-block border-b border-[#007076] leading-tight">Bank Details:</p>
+    <p className="text-[7px] text-gray-700 mb-[-5px]"><span className="font-semibold text-[#007076]">Bank Name: </span>MCB Islamic</p>
+    <p className="text-[7px] text-gray-700 mb-[-5px]"><span className="font-semibold text-[#007076]">Account Title: </span>Muhammad Rizwan</p>
+    <p className="text-[7px] text-gray-700"><span className="font-semibold text-[#007076]">Account Number: </span>1321004689390001</p>
+
+    <p className="text-[9px] font-semibold text-[#007076] mt-2 mb-1 inline-block border-b border-[#007076] leading-tight">Only Via Easy Paisa:</p>
+    <p className="text-[7px] text-gray-700 mb-[-5px]"><span className="font-semibold text-[#007076]">Account Title: </span>Muhammad Rizwan</p>
+    <p className="text-[7px] text-gray-700"><span className="font-semibold text-[#007076]">Mobile Number: </span>0343-4192648</p>
+
+    <p className="text-[9px] font-semibold text-[#007076] mt-2 mb-1 inline-block border-b border-[#007076] leading-tight">Payment Verification Link:</p><br/>
+    <p className="text-[6px] text-blue-600 underline leading-tight inline-block">
+  https://registration-form-kids.vercel.app/payment
+</p>
+
+    <p className="text-[7px] text-gray-700 leading-tight mt-3">
+      Payment submit kerny ke baad screenshot diye gaye link per upload karein aur 20-May se apni classes main shaamil ho jaein.</p><p className="text-[7px] text-gray-700 leading-tight mt-"> Mazeed details ke liye rabta karein 0343-4192648.
+    </p>
+
+    <p className="text-center text-sm font-semibold font-serif text-[#007076] mt-3">THANK YOU</p>
+  </div>
+</div>
+
+
       </div>
     </div>
   );
--- src/components/IdCardComponent/index.js
+++ src/components/IdCardComponent/index.js
@@ -131,84 +131,83 @@ const IdCardModal = ({ isOpen, onClose, user, contentImage }) => {
             contentLabel="Custom Modal"
         >
            <div style={{ position: "absolute", left: "-9999px", top: "-9999px" }}>
-        <div ref={idCardRef} className="relative pt-2 w-[350px] h-[500px]">
-          {/* First child div for the school image */}
-          <div className="mx-auto w-[180px] h-[120px] relative">
-            <div className="absolute w-[177px] h-[116px] border border-gray-900">
-              <Image
-                src="/images/Green Minimalist School ID Card.svg"
-                className="w-full h-full object-cover"
-                width={600}
-                height={400}
-                alt="School Image"
-              />
-            </div>
-
-            {/* Second child div for the rest of the ID card content */}
-            <div className="absolute w-[177px] h-[116px] z-10 overflow-hidden">
-              {/* User Image */}
-              <Image
-                className="absolute top-0 left-0 w-[24.8%] h-[44.7%] mt-[24%] ml-[6.3%]"
-                alt="User-Image"
-                src={image}
-                width={600}
-                height={400}
-              />
-
-              {/* User Details */}
-              <div className="absolute top-0 left-0 w-[37%] h-[35%] mt-[34.2%] ml-[53%] overflow-hidden">
-                <p
-                  className=" id-card-text"
-                >
-                  {user?.fullName}
-                </p>
-                <p
-
-                  className=" id-card-text"
-                >
-                  {user?.course}
-                </p>
-                <p
-
-                  className="id-card-text"
-                >
-                  {user?.batch}
-                </p>
-              </div>
-
-              {/* User Roll Number */}
-              <div className="absolute top-0 left-0 w-[20%] h-[10%] mt-[56%] ml-[18.5%] flex justify-center items-center">
-                <p
-                  style={{
-                    color: "white",
-                    fontSize: "7px",
-                    fontWeight: "bold",
-                    letterSpacing: "2px",
-                    fontStyle: "italic",
-                  }}
-                  className="break-words"
-                >
-                  {user?.rollNo}
-                </p>
-              </div>
-            </div>
-          </div>
-
-          {/* Content Image Section */}
-          <div className="w-[95%] h-[70%] mt-[10px] mx-auto">
-            {/* Add the Content Image here */}
-            {contentImage && (
-              <Image
-                src={contentImage}
-                className="w-full h-full object-cover"
-                alt="Content Image"
-                width={600}
-                height={400}
-              />
-
-            )}
-          </div>
-        </div>
+  <div ref={idCardRef} className="relative pt-1 w-[300px] h-[450px]">
+   {/* Top Heading */}
+   <h1 className="text-center text-xs font-serif font-semibold text-[#007076] mb-1">YOUR ID CARD</h1>
+ 
+   {/* ID Card Section */}
+   <div className="mx-auto w-[180px] h-[120px] relative">
+             <div className="absolute w-[177px] h-[116px] border border-gray-900">
+               <Image
+                 src="/images/Green Minimalist School ID Card (5).svg"
+                 className="w-full h-full object-cover"
+                 width={600}
+                 height={400}
+                 alt="School Image"
+               />
+             </div>
+ 
+             {/* Second child div for the rest of the ID card content */}
+             <div className="absolute w-[177px] h-[116px] z-10 overflow-hidden">
+               {/* User Image */}
+               <Image
+                 className="absolute top-0 left-0 w-[24.8%] h-[44.7%] mt-[24%] ml-[6.3%]"
+                 alt="User-Image"
+                 src={image}
+                 width={600}
+                 height={400}
+               />
+ 
+               {/* User Details */}
+               <div className="absolute top-0 left-0 w-[37%] h-[35%] mt-[34.5%] ml-[53%] overflow-hidden">
+                 <p className=" id-card-text font-poppins font-semibold">{user?.fullName}</p>
+                 <p className=" id-card-text font-poppins font-semibold">{user?.course}</p>
+                 <p className="id-card-text font-poppins font-semibold">{user?.batch}</p>
+               </div>
+ 
+               {/* User Roll Number */}
+               <div className="absolute top-0 left-0 w-[20%] h-[10%] mt-[56%] ml-[18.5%] flex justify-center items-center">
+                 <p
+                   style={{
+                     color: "white",
+                     fontSize: "7px",
+                     fontWeight: "bold",
+                     letterSpacing: "2px",
+                     fontStyle: "italic",
+                   }}
+                   className="break-words"
+                 >
+                   {user?.rollNo}
+                 </p>
+               </div>
+             </div>
+           </div>
+ 
+   {/* Payment Information */}
+   <div className="mt-2 py-1 px-3 text-xs text-gray-600">
+     <p className="text-center text-sm font-semibold font-serif text-[#007076] mb-1">PAYMENT METHOD</p>
+ 
+     <p className="text-[9px] font-semibold text-[#007076] mb-1 inline-block border-b border-[#007076] leading-tight">Bank Details:</p>
+     <p className="text-[7px] text-gray-700 mb-[-5px]"><span className="font-semibold text-[#007076]">Bank Name: </span>MCB Islamic</p>
+     <p className="text-[7px] text-gray-700 mb-[-5px]"><span className="font-semibold text-[#007076]">Account Title: </span>Muhammad Rizwan</p>
+     <p className="text-[7px] text-gray-700"><span className="font-semibold text-[#007076]">Account Number: </span>1321004689390001</p>
+ 
+     <p className="text-[9px] font-semibold text-[#007076] mt-2 mb-1 inline-block border-b border-[#007076] leading-tight">Only Via Easy Paisa:</p>
+     <p className="text-[7px] text-gray-700 mb-[-5px]"><span className="font-semibold text-[#007076]">Account Title: </span>Muhammad Rizwan</p>
+     <p className="text-[7px] text-gray-700"><span className="font-semibold text-[#007076]">Mobile Number: </span>0343-4192648</p>
+ 
+     <p className="text-[9px] font-semibold text-[#007076] mt-2 mb-1 inline-block border-b border-[#007076] leading-tight">Payment Verification Link:</p><br/>
+     <p className="text-[6px] text-blue-600 underline leading-tight inline-block">
+   https://registration-form-kids.vercel.app/payment
+ </p>
+ 
+     <p className="text-[7px] text-gray-700 leading-tight mt-3">
+       Payment submit kerny ke baad screenshot diye gaye link per upload karein aur 20-May se apni classes main shaamil ho jaein.</p><p className="text-[7px] text-gray-700 leading-tight mt-"> Mazeed details ke liye rabta karein 0343-4192648.
+     </p>
+ 
+     <p className="text-center text-sm font-semibold font-serif text-[#007076] mt-3">THANK YOU</p>
+   </div>
+ </div>
       </div>
             <p className="mt-0 font-boldest text-1xl text-gray-700 ml-0 mr-0 lg:ml-[9%] md:ml-[9%] sm:ml-[9%] lg:mr-[9%] md:mr-[9%] sm:mr-[9%] lg:text-center md:text-center sm:text-center" style={{fontWeight: "bold",}}>For your Registration Details, Download ID Card!</p>
             <div className="flex justify-end mt-0">
